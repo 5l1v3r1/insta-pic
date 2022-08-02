083cfbf86607d01b7c420895472f8bd4e295afde
Furkan Kerem Özdemir <securist0x01@gmail.com>
2022-08-03 01:11:00 +0300

    Update instaresim.py

--- instaresim.py
+++ instaresim.py
@@ -1,12 +1,32 @@
 from instabot import Bot
 import os
+from colorama import Fore
+import pyfiglet
 os.system("clear")
-#coded by saep
 bot = Bot()
-user = input("Kullanıcı Adı : ")
-passwd = input("Şifre : ")
-resim = input("Resim Yolu [Örn /root/aa.jpg] : ")
-aciklama = input("Açıklama : ")
+out = pyfiglet.figlet_format("InstaBot")
+print(Fore.RED+out)
+print(Fore.CYAN+ """
+         ooo
+        / : \\
+       / o0o \\
+ _____"~~~~~~~"_____
+ \+###|U * * U|###+/
+  \...!(.>..<)!.../
+   ^^^^o|   |o^^^^
++=====}:^^^^^:{=====+#
+.____  .|!!!|.  ____.
+|#####:/" " "\:#####|
+|#####=|  O  |=#####|
+|#####>\_____/<#####|
+ ^^^^^   | |   ^^^^^
+         o o
+         CODED BY SECURIST0x01
+""")
+user = input(Fore.MAGENTA+"Kullanıcı Adı : ")
+passwd = input(Fore.MAGENTA+"Şifre : ",)
+resim = input(Fore.YELLOW+"Resim Yolu [Örn /root/aa.jpg] : ")
+aciklama = input(Fore.WHITE+"Açıklama : ")
 bot.login(username=user, password=passwd)
 
 bot.upload_photo(resim, caption=aciklama)
